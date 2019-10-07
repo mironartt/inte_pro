61f03704816c1c3d82a3cce412f75be146e700f5
a.mironov <a.mironov@sape.ru>
2019-10-08 02:21:25 +0300

    base start

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # inte_pro
+
+CREATE DATABASE inte_pro_db;
+CREATE USER inte_pro_db_user WITH password 'inte_pro_db_password';
+GRANT ALL ON DATABASE inte_pro_db TO inte_pro_db_user;
---
+++ core/views/index.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.views import generic
+from django.shortcuts import render, redirect, render_to_response, get_object_or_404
+
+
+class IndexView(generic.View):
+
+    def get(self, request, *args, **kwargs):
+        context = {
+            'test':'ZAEBUMBA!!!!!!!!!!!!!!!'
+        }
+
+        return render(request, 'index.html', context)
